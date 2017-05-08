980a9beab91e5d088eef8342e1184a7f8339d187
SANTOSH HUMNEKAR <santosh.humnekar@gmail.com>
2017-05-08 11:17:21 +0530

    Add files via upload

---
+++ methodDecorator.js
@@ -0,0 +1,24 @@
+let __decorate = (this && this.__decorate) || function(decorators, target, key, desc) {
+    let c = arguments.length,
+        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
+        d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else
+        for (let i = decorators.length - 1; i >= 0; i--)
+            if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+function MethodDecorator(target, // The prototype of the class
+    propertyKey, // The name of the method
+    descriptor) {
+    console.log("MethodDecorator called on: ", target, propertyKey, descriptor);
+}
+let MethodDecoratorExample = (function() {
+    function MethodDecoratorExample() {}
+    MethodDecoratorExample.prototype.method = function() {};
+    return MethodDecoratorExample;
+}());
+__decorate([
+    MethodDecorator
+], MethodDecoratorExample.prototype, "method", null);
